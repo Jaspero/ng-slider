e43916ed0d0378caa5db036411d6c89828f32033
Matej Maloča <maloca1992@gmail.com>
2021-01-04 12:43:54 +0100

    chore: dummy push

--- projects/ng-slider/src/lib/interfaces/slider-options.interface.ts
+++ projects/ng-slider/src/lib/interfaces/slider-options.interface.ts
@@ -4,6 +4,7 @@ export interface SliderOptions {
   movesPerClick?: number;
   initialSlide?: number;
   loop?: boolean;
+
   /**
    * Enables transition between first and last slide.
    * NOTE?: Be aware of bad performance on mobile phones
